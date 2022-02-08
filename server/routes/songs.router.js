976d68337e681aa00c599cd5dddd06400de4db6f
Clay Webb <clay_webb@outlook.com>
2022-02-08 14:52:39 -0600

    Hooked up pg pool and wrote GET route

--- server/routes/songs.router.js
+++ server/routes/songs.router.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+// Connect to PG
+const pg = require('pg');
+
+// For talking to the database
+const Pool = pg.Pool;
+
+// An instance of a new 'Pool' that will be used in connecting with the database
+const pool = new Pool({
+    database: 'music_library', // The name of the database
+    host: 'Localhost', // Where our database is
+    port: 5432, // Port number for database
+    max: 10, // The max amount of connections (queries) you can have at one time
+    idleTimeoutMillis: 30000 // 30 seconds to try to connect, then cancel query
+});
+
+// is not required, but useful for debugging
+pool.on('connect', () => {
+    console.log('PostgreSQL is connected!');
+});
+// The pool will emit an error on behalf of any idle clients
+// Will be on the terminal
+pool.on('error', (error) => {
+    console.log('Error with PostgreSQL:', error);
+});
+
 let songs = [
     {
         rank: 355, 
@@ -23,7 +48,17 @@ let songs = [
 ];
 
 router.get('/', (req, res) => {
-    res.send(songs);
+    // res.send(songs);
+    // Check SQL query text in postico first!
+    let queryText = 'SELECT * FROM "songs";';
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows)
+        })
+        .catch((err) => {
+            console.log('Error making query', queryText, err);
+            res.sendStatus(500);
+        });
 });
 
 router.post('/', (req, res) => {
